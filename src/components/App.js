4bbc388a9e39a2efe5661fdee2e96bc5d92894b4
unknown <anujraghuwanshi2407@gmail.com>
2023-08-17 19:02:02 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
 import React from "react";
 import './../styles/App.css';
+import Form from "./Form";
+
+const Fields = [
+    {name: "name" , type: "text", label: "Name" },
+    {name: "email" , type: "email", label: "Email" },
+    {name: "password" , type: "password", label: "Password" }
+]
 
 const App = () => {
+
+  function handleSubmit(value){
+    console.log(value)
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <Form Fields = {Fields} handleSubmit={handleSubmit} ></Form>
     </div>
   )
 }
---
+++ src/components/Form.js
@@ -0,0 +1,41 @@
+import React , {useState} from "react";
+
+
+const Form = ({Fields , handleSubmit}) => {
+  const [formValue ,  setFormValue] = useState({})
+   
+//  function handleInputChanges(e){
+//     console.log(e.target)
+//     const { name, value } = e.target;
+//     console.log(name)
+//     console.log(value)
+//     // (e)=>setFormValue(...formValue , element.label = e.target.value)
+//  }
+
+function handleInput(e , key){
+    setFormValue({...formValue ,  [key] :   e.target.value})
+}
+  function handleForm(e){
+   e.preventDefault();
+   handleSubmit(formValue)
+  }
+    return(
+        <form onSubmit={handleForm}>
+           {
+           Fields.map(element =>(
+                  <div>
+
+                        <label for = {element.name}>{element.label}</label>  
+                         <input type={element.type} onChange={e => handleInput(e , element.label)}  /> 
+                  </div>
+                        
+              
+           )
+           )}
+           <button type="submit">Submit</button>
+        </form>
+    )
+
+}
+
+export default Form 
